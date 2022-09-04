a66163e6afae11ec42a9b26907bacbfc0b21827f
Gvvon5ewon <gwonsewon03@gmail.com>
2022-09-04 22:25:26 +0900

    0904

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdIRFIEzbwA5oJjLjI62S_q3fgfgAKWtw",
+    authDomain: "nwitter-2fb14.firebaseapp.com",
+    projectId: "nwitter-2fb14",
+    storageBucket: "nwitter-2fb14.appspot.com",
+    messagingSenderId: "560157128517",
+    appId: "1:560157128517:web:4de7dc5d2b1ce6b2d6f02c",
+    measurementId: "G-3K2RT1XCZ0"
+  };
+  export default firebase.initializeApp(firebaseConfig);
